7687d9a5b62ad8f2b4b9267ca1e50fd2697ce919
swoole <577429696@qq.com>
2018-10-25 21:47:46 +0800

    swoole_process多进程curl网页

--- process/curldata.php
+++ process/curldata.php
@@ -5,12 +5,25 @@
  * Date: 2018/10/23
  * Time: 20:54
  */
-    $ch = curl_init();
-    // set url
-    curl_setopt($ch, CURLOPT_URL, "www.baidu.com");
-    //return the transfer as a string
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    // $output contains the output string
-    $output = curl_exec($ch);
-    //echo output
-    echo $output;
+
+class curldata{
+
+    public function __construct($str,$i)
+    {
+        $ch = curl_init();
+        // set url
+        curl_setopt($ch, CURLOPT_URL, $str);
+        //return the transfer as a string
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        //响应过期时间设置
+        curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,5);
+        //返回301 Move Permanentl 永久迁移.是因为遇到要跳转的页面.设置为直接抓取跳转的地址.
+        curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
+
+        $output = curl_exec($ch);
+        swoole_async_writefile(__DIR__."/../../{$str}.html",$output,function ($filename){
+
+        });
+        curl_close($ch);
+    }
+}
