5d494ed99376e95da0e3a94901e5f2e387f5a54a
swoole <577429696@qq.com>
2018-11-16 16:25:27 +0800

    Add sendLive function

--- thinkphp/application/common/lib/task/Task.php
+++ thinkphp/application/common/lib/task/Task.php
@@ -6,26 +6,56 @@
  * Date: 2018/11/6
  * Time: 19:42
  */
+
 namespace app\common\lib\task;
+
 use app\common\lib\ali\Sms;
 use app\common\lib\redis\Predis;
 use app\common\lib\Prefix;
 use app\common\lib\Util;
-class Task{
 
-    public function sendSms($data){
-        try{
-            $res = Sms::sendSms($data['phone'],$data['code']);
-        }catch (\Exception $e){
+class Task
+{
+
+    /**
+     * 发送短信
+     * @param $data
+     * @return bool
+     * @param $server 服务端对象
+     */
+    public function sendSms($data, $server)
+    {
+        try {
+            $res = Sms::sendSms($data['phone'], $data['code']);
+        } catch (\Exception $e) {
             echo $e->getMessage();
             return false;
         }
 
-        if($res->Code === 'OK'){
-            Predis::getInstance()->set(Prefix::smsKey($data['phone']),$data['code'],config('redis.out_time'));
-        }else{
+        if ($res->Code === 'OK') {
+            Predis::getInstance()->set(Prefix::smsKey($data['phone']), $data['code'], config('redis.out_time'));
+        } else {
             return false;
         }
-        return  true;
+        return true;
+    }
+
+    /**
+     * @param $data
+     * @param $server
+     * @return array
+     */
+    public function sendLive($data, $server)
+    {
+        $client = \app\common\lib\redis\Predis::getInstance()
+            ->sMember('live');
+        foreach ($client as $fd) {
+            //可能会出现服务端关闭时客户端关闭 redis数据清楚出现操作异常
+            try {
+                $server->push($fd, json_encode($data));
+            } catch (\Exception $e) {
+
+            }
+        }
     }
 }
